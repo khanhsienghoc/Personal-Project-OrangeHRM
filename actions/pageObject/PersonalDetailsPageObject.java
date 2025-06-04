b6b534fb7540a859e7e6d3f0573eb4fcb4368502
Khanh <khanhhhh245@gmail.com>
2025-06-04 13:09:02 +0700

    Update .gitignore and PersonalDetailsPageObject

--- actions/pageObject/PersonalDetailsPageObject.java
+++ actions/pageObject/PersonalDetailsPageObject.java
@@ -6,6 +6,7 @@ import interfaces.MyInfoPageUI;
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 
+
 public class PersonalDetailsPageObject extends BasePage {
     private final WebDriver driver;
 
@@ -25,14 +26,29 @@ public class PersonalDetailsPageObject extends BasePage {
     }
     public void fillPersonalDetailsForm(PersonalDetailsData data) {
         log.info("Filling personal details form with new data");
+
         log.info("Input First Name with value {}", data.getFirstName());
         inputToTextBoxByName(driver, "firstName", data.getFirstName());
+
+        log.info("Input Middle  Name with value {}", data.getMiddleName());
         inputToTextBoxByName(driver, "middleName", data.getMiddleName());
+
+        log.info("Input Last  Name with value {}", data.getLastName());
         inputToTextBoxByName(driver, "lastName", data.getLastName());
+
+        log.info("Input Other ID with value {}", data.getOtherID());
         inputToTextBoxByText(driver, "Other Id", data.getOtherID());
+
+        log.info("Input License Expiry Date with value {}", data.getOtherID());
         inputToTextBoxByText(driver, "License Expiry Date", data.getLicenseExpiryDate());
+
+        log.info("Choose Nationality dropdown with value {}", data.getNationality());
         selectValueInDropdownByText(driver, "Nationality", data.getNationality());
+
+        log.info("Choose Marital Status dropdown with value {}", data.getMaritalStatus());
         selectValueInDropdownByText(driver, "Marital Status", data.getMaritalStatus());
+
+        log.info("Choose Gender with value {}", data.getMaritalStatus());
         clickToRadioButtonByText(driver, data.getGender());
     }
 }
